f25ef22fc9fdd2dd08fa092e72c38f434a7bd2b6
SebEdena <viguier.sebastien@orange.fr>
2019-06-15 12:30:34 +0200

    testing messages

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+// const token = process.env.DISCORD_BOT_SECRET;
 const broadcast = client.createVoiceBroadcast();
+const prefix = "$";
+
+client.once('ready', () => {
+    console.log('connected');
+});
+
+client.on("guildCreate", guild => {
+    guild.defaultChannel.send("Hello Guys !");
+});
+
+client.on('message', async message => {
+
+    if(message.substring(0, prefix.length) === prefix){
+        message.reply(`${prefix} prefix detected !`);    
+    }
+});
