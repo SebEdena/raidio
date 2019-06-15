23ec3fe368b174f3578ba7acff525e02246d01f6
SebEdena <viguier.sebastien@orange.fr>
2019-06-15 14:16:28 +0200

    welcome message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
-// const token = process.env.DISCORD_BOT_SECRET;
+const token = process.env.token;
 const broadcast = client.createVoiceBroadcast();
 const prefix = "$";
 
@@ -8,13 +9,30 @@ client.once('ready', () => {
     console.log('connected');
 });
 
-client.on("guildCreate", guild => {
-    guild.defaultChannel.send("Hello Guys !");
-});
+client.on("guildCreate", async guild => hello_world(guild, client));
 
 client.on('message', async message => {
 
-    if(message.substring(0, prefix.length) === prefix){
+    if(message.toString().substring(0, prefix.length) === prefix){
         message.reply(`${prefix} prefix detected !`);    
     }
 });
+
+console.log(token);
+client.login(token);
+
+function hello_world(guild, bot){
+    let channelID;
+    let channels = guild.channels;
+    channelLoop:
+    for (let c of channels) {
+        let channelType = c[1].type;
+        if (channelType === "text") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+
+    let channel = bot.channels.get(guild.systemChannelID || channelID);
+    channel.send(`Thanks for inviting me into this server!`);
+}
